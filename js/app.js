d4a291f733a6a5ccb3b6a7f75d90a528822bee63
zeeshanquamer <zeesh654@gmail.com>
2023-07-19 16:31:08 +0530

    project start

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const burger = document.querySelector(".burger");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const navHeader = document.querySelector(".nav-header");
+const links = document.querySelector(".links");
+const navLinks = document.querySelector(".nav-links");
+burger.addEventListener("click", openMenu);
+function openMenu() {
+  line1.classList.toggle("active");
+  line2.classList.toggle("active");
+  navHeader.classList.toggle("active");
+  links.classList.toggle("active");
+  navLinks.classList.toggle("active");
+}
+let slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
